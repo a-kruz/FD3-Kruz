7faa6f2f4af4263550110e1f7a3cc864a92662fa
Alex Kruz <kruzav@gmail.com>
2023-01-28 02:23:13 +0300

    component Products added

---
+++ ishop/Products.js
@@ -0,0 +1,52 @@
+var Products = React.createClass({
+    
+  displayName: 'Products',
+
+  propTypes: {
+    title: React.PropTypes.string,
+    currency: React.PropTypes.string.isRequired,
+    listHead: React.PropTypes.arrayOf(
+      React.PropTypes.shape({
+        code: React.PropTypes.number.isRequired,
+        title: React.PropTypes.string.isRequired,
+      }),
+    ),
+    list: React.PropTypes.arrayOf(
+      React.PropTypes.shape({
+        code: React.PropTypes.string.isRequired,
+        title: React.PropTypes.string.isRequired,
+        price: React.PropTypes.number.isRequired,
+        imgUrl: React.PropTypes.string.isRequired,
+        stock: React.PropTypes.number.isRequired,
+      }),
+    ),
+  },
+
+  render: function() {
+
+    var productsListHead = this.props.listHead.map(item => 
+      React.DOM.td({key: item.code}, item.title)
+    );
+
+    var productsList = this.props.list.map(item => 
+      React.DOM.tr({key: item.code, className: 'Products__item'},
+        React.DOM.td({className: 'Products__item-code'}, item.code),
+        React.DOM.td({className: 'Products__item-title'}, item.title),
+        React.DOM.td({className: 'Products__item-price'}, item.price+this.props.currency),
+        React.DOM.td({className: 'Products__item-img'},
+          React.DOM.img({src: item.imgUrl, alt: item.title}, null)
+        ),
+        React.DOM.td({className: 'Products__item-stock'}, item.stock),
+      )
+    );
+
+    return React.DOM.div(
+      {className: 'Products'},
+      React.DOM.h1({className: 'Products__title'}, this.props.title),
+      React.DOM.table({className: 'Products__table'},
+        React.DOM.thead({className: 'Products__thead'}, productsListHead),
+        React.DOM.tbody({className: 'Products__tbody'}, productsList),
+      ),
+    );
+  }
+});
